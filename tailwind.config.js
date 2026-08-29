d5fbca042f3a7e8251fa3f90bb5402c1f2ac72cf
Rebbe33 <gaboriaud.r@gmail.com>
2026-08-29 13:24:25 +0200

    Extend Tailwind CSS configuration with custom themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        paper: '#EFEAE1',
+        ink: '#2B2A28',
+        moss: {
+          DEFAULT: '#4A6B4E',
+          dark: '#38512C',
+          light: '#DCE5D8',
+        },
+        amber: {
+          DEFAULT: '#C97B3D',
+          light: '#F3E1CC',
+        },
+        rust: '#B5502B',
+        line: '#DAD3C5',
+      },
+      fontFamily: {
+        display: ['Fraunces', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+        mono: ['"IBM Plex Mono"', 'monospace'],
+      },
+      borderRadius: {
+        xl2: '20px',
+      },
+    },
   },
   plugins: [],
 }
