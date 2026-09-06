5d41afafffc02aa1c17845419de925c10822a85f
Rebbe33 <gaboriaud.r@gmail.com>
2026-09-06 12:45:22 +0200

    Add constants for lieu and materiel options

---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+export const LIEU_OPTIONS = [
+  { value: 'indifferent', label: 'Indifférent' },
+  { value: 'interieur', label: 'Intérieur' },
+  { value: 'exterieur', label: 'Extérieur' },
+] as const
+
+export const MATERIEL_OPTIONS = [
+  'Laisse',
+  'Longe',
+  'Jouet',
+  'Corde',
+  'Cible/bâton',
+  'Gamelle',
+  'Couverture',
+  'Harnais',
+]
